2cc54df07de57e02c218fb844b7d636ccafa5b94
99003733 <78867415+99003733@users.noreply.github.com>
2021-02-20 13:20:27 +0530

    added

---
+++ calcvolume.c
@@ -0,0 +1,14 @@
+#include"calcvolume.h"
+#include<stdio.h>
+#include<string.h>
+
+
+voidvolume_of_box(Boxes *box)
+{
+    unsigned long volume;
+    boxes* ptr;
+    box->volume = (box->length)*(box->breadth)*(box->height);   
+
+}
+
+
